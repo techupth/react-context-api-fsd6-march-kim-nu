61fa960ba8a571a53804882ab2d771cde57ea18c
Kunakorn <kunakorn.ph98@gmail.com>
2024-06-14 13:53:15 +0700

    Assignment#1 : React context API

--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,12 @@
+import { UserDataContaxt } from "../App";
+import { useContext } from "react";
+
 function AppHeader() {
+  const name = useContext(UserDataContaxt)
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {name.username}</h2>
     </div>
   );
 }
